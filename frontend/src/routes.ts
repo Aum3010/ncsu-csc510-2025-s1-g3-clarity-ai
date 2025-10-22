da965e4c9f5d51b0ad513d38ef7fdd8e0f390c28
apandya4_ncstate <apandya4@ncsu.edu>
2025-10-22 09:31:24 -0400

    rm comment

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -8,17 +8,11 @@ import {
 
 
 export default [
-  // The Root Layout (This will be the App component structure, hosting the Sidebar)
   layout("./App.jsx", [
     
-    // The Dashboard Index Route (The default page rendered at /)
     index("./views/RequirementsDashboard.jsx"),
-
-    // Other Navigation Links (Placeholder)
     route("documents", "./views/Documents.jsx"),
     route("integrations", "./views/Integrations.jsx"),
     route("team", "./views/Team.jsx"),
-
-    // route("requirement/:reqId", "./views/RequirementDetail.jsx"),
   ]),
 ] satisfies RouteConfig;
